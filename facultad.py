685226db24b352b815ffb639a5d218c7c72b47f1
jmarquez-urq <jmarquez.urquiza@gmail.com>
2021-08-21 12:27:48 -0300

    Determina si el estudiante está aprobado o no

--- facultad.py
+++ facultad.py
@@ -9,6 +9,10 @@ promedio = (n1 + n2 + n3) / 3
 # Mostrar el promedio
 print("Promedio:", promedio)
 # Determinar si aprobó
+if promedio < 6 or n3 < 6:
+    situacion = "Reprobado"
+else:
+    situacion = "Aprobado"
 
 # Mostrar si aprobó
 
