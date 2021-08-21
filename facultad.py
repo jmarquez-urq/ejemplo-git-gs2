df6a1703ea9bd39877c6076f2b75c6fe9b344ae0
jmarquez-urq <jmarquez.urquiza@gmail.com>
2021-08-21 12:22:41 -0300

    Ingresa notas, calcula y muestra promedio

--- facultad.py
+++ facultad.py
@@ -1,11 +1,13 @@
 #! /usr/bin/python3
 
 # Permitir que se ingresen las 3 notas
-
+n1 = int(input("Ingrese la primera nota"))
+n2 = int(input("Ingrese la segunda nota"))
+n3 = int(input("Ingrese la tercera nota"))
 # Calcular el promedio
-
+promedio = (n1 + n2 + n3) / 3
 # Mostrar el promedio
-
+print("Promedio:", promedio)
 # Determinar si aprobó
 
 # Mostrar si aprobó
